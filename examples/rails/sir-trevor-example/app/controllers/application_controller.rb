c745c4cce82c8e4aa0cc7c8246d5a599e4b8dd88
Chris Bell <cjbell1988@gmail.com>
2012-08-17 13:33:25 +0100

    Added Sir Trevor example application

---
+++ examples/rails/sir-trevor-example/app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  require 'sir-trevor-rails'
+end
---
+++ examples/rails/sir-trevor-example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SirTrevorExample::Application.config.secret_token = '2462882669cbe56d4155bbcfc72fc40204d434f0219b41ac091ef7d532d1dec7cc28a789f7d0b54da77f153987d2784d7c1c55c7d34dd546f7d7cc67fd44e1f6'
