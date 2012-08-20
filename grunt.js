80427907323b30e8c6f85e10f33de26101e321dd
Chris Bell <cjbell1988@gmail.com>
2012-08-20 14:31:23 +0100

    Modified version no

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     
     meta: {
-      version: '0.0.1',
+      version: '0.1.0',
       banner: '// Sir Trevor, v<%= meta.version %>\n'
     },
 
