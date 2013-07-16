165a79adaa01942f5c477b7fcc284517ac39bd67
Chris Bell <cjbell1988@gmail.com>
2013-07-16 14:46:09 +0100

    Added relative assets

--- website/config.rb
+++ website/config.rb
@@ -56,9 +56,7 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
