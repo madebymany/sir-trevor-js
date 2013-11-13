4a4806b09c23f773132654d76bba64d0290bf488
Raffi <raffij@gmail.com>
2013-11-13 11:25:18 +0000

    Modified console.log to window.console.log to allow js to be included in ie8.

--- sir-trevor.js
+++ sir-trevor.js
@@ -230,8 +230,8 @@
   */
   
   SirTrevor.log = function(message) {
-    if (!_.isUndefined(console) && SirTrevor.DEBUG) {
-      console.log(message);
+    if (!_.isUndefined(window.console) && SirTrevor.DEBUG) {
+      window.console.log(message);
     }
   };
   SirTrevor.Locales = {
