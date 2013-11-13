95f15989d5bd2a3814e1a7ff0cf8ab452810151a
Raffi <raffij@gmail.com>
2013-11-13 11:11:16 +0000

    Quoted class param names

--- sir-trevor.js
+++ sir-trevor.js
@@ -1294,7 +1294,7 @@
   
       type: '',
   
-      class: function() {
+      'class': function() {
         return _.classify(this.type);
       },
   
@@ -1356,7 +1356,7 @@
       },
   
       addMessage: function(msg, additionalClass) {
-        var $msg = $("<span>", { html: msg, class: "st-msg " + additionalClass });
+        var $msg = $("<span>", { html: msg, 'class': "st-msg " + additionalClass });
         this.$messages.append($msg)
                       .addClass('st-block__messages--is-visible');
         return $msg;
