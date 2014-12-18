83ec49a304ad1edecc3beecad72e13e031806e97
Dan Brown <dan@madebymany.co.uk>
2014-12-18 17:23:13 +0000

    Configure BrowserStack building for CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 //
 module.exports = function(config) {
+  var enableBrowserStack = !!(process.env.BROWSERSTACK_USER && process.env.BROWSERSTACK_AUTHKEY);
+
   config.set({
 
     // base path, that will be used to resolve files and exclude
@@ -44,8 +46,22 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
-    //browsers: ['Chrome'],
+    browsers: enableBrowserStack ? ['BrowserStackChrome'] : ['Chrome'],
+
+    customLaunchers: {
+      BrowserStackChrome: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: 'latest',
+        os: 'OS X',
+        os_version: 'Yosemite'
+      },
+    },
+
+    browserStack: {
+      username: process.env.BROWSERSTACK_USER,
+      accessKey: process.env.BROWSERSTACK_AUTHKEY,
+    },
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
