a4812593f3efdf0c0427b0c29232ea51aada755b
Dan Brown <dan@madebymany.co.uk>
2015-01-08 12:26:23 +0000

    Fixed toHTML test for <p> tags

--- spec/javascripts/units/to_html.spec.js
+++ spec/javascripts/units/to_html.spec.js
@@ -6,7 +6,7 @@ describe("toHTML", function(){
     var markdown = "[test](http://google.com)",
         html = SirTrevor.toHTML(markdown, "Text");
 
-    expect(html).toBe("<div><a href='http://google.com'>test</a></div>");
+    expect(html).toBe("<p><a href='http://google.com'>test</a></p>");
   });
 
   it("converts blockquotes to HTML", function(){
@@ -20,21 +20,21 @@ describe("toHTML", function(){
     var markdown = '_test_',
         html = SirTrevor.toHTML(markdown, "Text");
 
-    expect(html).toBe("<div><i>test</i></div>");
+    expect(html).toBe("<p><i>test</i></p>");
   });
 
   it("converts bolds to HTML", function(){
     var markdown = "**test**",
         html = SirTrevor.toHTML(markdown, "Text");
 
-    expect(html).toBe("<div><b>test</b></div>");
+    expect(html).toBe("<p><b>test</b></p>");
   });
 
   it("converts bold and italics to HTML", function(){
     var markdown = "**test** and _testing_",
         html = SirTrevor.toHTML(markdown, "Text");
 
-    expect(html).toBe("<div><b>test</b> and <i>testing</i></div>");
+    expect(html).toBe("<p><b>test</b> and <i>testing</i></p>");
   });
 
   it("doesn't mess up on links with _ in", function(){
@@ -48,83 +48,83 @@ describe("toHTML", function(){
     var md = "Da**id backfire**",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div>Da<b>id backfire</b></div>");
+    expect(html).toBe("<p>Da<b>id backfire</b></p>");
   });
 
   it("converts an italic in the middle of a word", function(){
     var md = "Da_id backfire_",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div>Da<i>id backfire</i></div>");
+    expect(html).toBe("<p>Da<i>id backfire</i></p>");
   });
 
   it("converts double sets of italics correctly", function(){
     var md = "_test__test_",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><i>test</i><i>test</i></div>");
+    expect(html).toBe("<p><i>test</i><i>test</i></p>");
   });
 
   it("converts double sets of bolds correctly", function(){
     var md = "**test****test**",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><b>test</b><b>test</b></div>");
+    expect(html).toBe("<p><b>test</b><b>test</b></p>");
   });
 
   it("correctly encodes dashes", function(){
     var md = "Hand-crafted",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div>Hand-crafted</div>");
+    expect(html).toBe("<p>Hand-crafted</p>");
   });
 
   it("strips newlines in bold tags", function(){
     var md = "**Test\n\n**",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><b>Test</b></div>");
+    expect(html).toBe("<p><b>Test</b></p>");
   });
 
   it("strips newlines in italic tags", function(){
     var md = "_Test\n\n_",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><i>Test</i></div>");
+    expect(html).toBe("<p><i>Test</i></p>");
   });
 
   it("strips newlines in links", function(){
     var md = "[Test\n\n](http://google.com)",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><a href='http://google.com'>Test</a></div>");
+    expect(html).toBe("<p><a href='http://google.com'>Test</a></p>");
   });
 
   it("strips preceding spaces in italic tags", function(){
     var md = "_ Test_",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><i>Test</i></div>");
+    expect(html).toBe("<p><i>Test</i></p>");
   });
 
   it("will ignore encoded italic tags", function(){
     var md = "_Test\\_",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div>_Test_</div>");
+    expect(html).toBe("<p>_Test_</p>");
   });
 
   it("strips preceding spaces in bold tags", function(){
     var md = "** Test**",
         html = SirTrevor.toHTML(md, "Text");
 
-    expect(html).toBe("<div><b>Test</b></div>");
+    expect(html).toBe("<p><b>Test</b></p>");
   });
 
   it("cleans up tabs", function(){
     var md = "\t",
         html = SirTrevor.toHTML(md, "Text");
 
-        expect(html).toBe("<div>&nbsp;&nbsp;&nbsp;&nbsp;</div>");
+        expect(html).toBe("<p>&nbsp;&nbsp;&nbsp;&nbsp;</p>");
   });
 });
