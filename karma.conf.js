9428a5e865d25fb7e4da0a5662282b5058766386
Lukasz Sagol <lukasz@sagol.pl>
2015-03-31 14:56:36 +0100

    Using PhantomJS for running Unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
