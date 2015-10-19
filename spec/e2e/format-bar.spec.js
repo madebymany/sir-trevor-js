1a9f9891c05634c5c4e813d1bcee1fc05f3d70b0
James Thurley <mail@jamesthurley.com>
2015-10-19 16:47:16 +0100

    Update e2e test for new formatting options.

--- spec/e2e/format-bar.spec.js
+++ spec/e2e/format-bar.spec.js
@@ -13,7 +13,7 @@ describe('Format Bar', function() {
         {
           "type": "text",
           "data": {
-            "text": "Bold, Italic, Link"
+            "text": "Bold, Italic, Link, Underline, Strikethrough\\n\\nH1\\n\\nH2\\n\\nUL\\n\\nOL\\n\\nQuote"
           }
         }
       ]
@@ -31,8 +31,9 @@ describe('Format Bar', function() {
   it('should allow formatting of text', function(done) {
 
     var testUrl = 'http://www.example.com';
-    var expectedHtmlWithoutLink = '<p><b>Bold</b>, <i>Italic</i>, Link</p>';
-    var expectedFullHtml = '<p><b>Bold</b>, <i>Italic</i>, <a href="' + testUrl + '">Link</a></p>';
+    var expectedHtmlUpToLink = '<p><b>Bold</b>, <i>Italic</i>, <a href="' + testUrl + '">Link</a>, Underline, Strikethrough</p><p>H1</p><p>H2</p><p>UL</p><p>OL</p><p>Quote</p>';
+    var expectedHtmlWithLinkRemoved = '<p><b>Bold</b>, <i>Italic</i>, Link, Underline, Strikethrough</p><p>H1</p><p>H2</p><p>UL</p><p>OL</p><p>Quote</p>';
+    var expectedFinalHtml = '<p><b>Bold</b>, <i>Italic</i>, Link, <u>Underline</u>, <strike>Strikethrough</strike></p><h1>H1</h1><h2>H2</h2><ul><li>UL<br></li></ul><ol><li>OL<br></li></ol><blockquote><p>Quote</p></blockquote>';
 
     function selectBoldText() {
       return helpers.browser.actions()
@@ -67,18 +68,132 @@ describe('Format Bar', function() {
 
     function selectLinkText() {
       return helpers.browser.actions()
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .keyDown(driver.Key.SHIFT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .sendKeys(driver.Key.ARROW_RIGHT)
-                .keyUp(driver.Key.SHIFT)
-                .perform().then( function() {
-                  return helpers.findElementByCss('.st-format-btn--Link').click();
-                });
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Link').click();
+        });
+    }
+
+    function selectUnderlineText() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Underline').click();
+        });
+    }
+
+    function selectStrikethroughText() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Strikethrough').click();
+        });
+    }
+
+    function selectH1Text() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Heading1').click();
+        });
+    }
+
+    function selectH2Text() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Heading2').click();
+        });
+    }
+
+    function selectUnorderedListText() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--UnorderedList').click();
+        });
+    }
+
+    function selectOrderedListText() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--OrderedList').click();
+        });
+    }
+
+    function selectQuoteText() {
+      return helpers.browser.actions()
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyDown(driver.Key.SHIFT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .sendKeys(driver.Key.ARROW_RIGHT)
+        .keyUp(driver.Key.SHIFT)
+        .perform().then( function() {
+          return helpers.findElementByCss('.st-format-btn--Blockquote').click();
+        });
     }
 
     var parent;
@@ -96,12 +211,23 @@ describe('Format Bar', function() {
     }).then( function() {
       return parent.getInnerHtml();
     }).then( function(html) {
-      expect(html).toBe(expectedFullHtml);
+      expect(html).toBe(expectedHtmlUpToLink);
       return helpers.findElementByCss('.st-format-btn--Unlink').click();
     }).then( function() {
       return parent.getInnerHtml();
     }).then( function(html) {
-      expect(html).toBe(expectedHtmlWithoutLink);
+      expect(html).toBe(expectedHtmlWithLinkRemoved);
+    }).then(selectUnderlineText)
+      .then(selectStrikethroughText)
+      .then(selectH1Text)
+      .then(selectH2Text)
+      .then(selectUnorderedListText)
+      .then(selectOrderedListText)
+      .then(selectQuoteText)
+      .then( function() {
+        return parent.getInnerHtml();
+    }).then( function(html) {
+      expect(html).toBe(expectedFinalHtml);
       done();
     });
   });
