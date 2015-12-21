b7b7497e72b0951f23da4589648f2d49efe2eb4a
Raffi <raffij@gmail.com>
2015-12-21 17:46:48 +0000

    Remove st-required class from quote block citations

--- src/blocks/quote.js
+++ src/blocks/quote.js
@@ -13,7 +13,7 @@ var template = _.template([
   '<blockquote class="st-required st-text-block" contenteditable="true"></blockquote>',
   '<label class="st-input-label"> <%= i18n.t("blocks:quote:credit_field") %></label>',
   '<input maxlength="140" name="cite" placeholder="<%= i18n.t("blocks:quote:credit_field") %>"',
-  ' class="st-input-string st-required js-cite-input" type="text" />'
+  ' class="st-input-string js-cite-input" type="text" />'
 ].join("\n"));
 
 module.exports = Block.extend({
