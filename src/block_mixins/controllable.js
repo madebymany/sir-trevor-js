77ccc0444ecd691ae374605be9d3c9aa7b8de2a4
Raffi <raffij@gmail.com>
2016-02-24 21:56:29 +0000

    Fix controllable icons

--- src/block_mixins/controllable.js
+++ src/block_mixins/controllable.js
@@ -22,10 +22,13 @@ module.exports = {
   },
 
   getControlTemplate: function(cmd) {
-    return Dom.createElement("a",
-      { 'data-icon': cmd,
-        'class': 'st-icon st-block-control-ui-btn st-block-control-ui-btn--' + cmd
-      });
+    return Dom.createElement("a", {
+      'data-icon': cmd,
+      'class': 'st-icon st-block-control-ui-btn st-block-control-ui-btn--' + cmd,
+      'html': `<svg role="img" class="st-icon">
+                  <use xlink:href="${config.defaults.iconUrl}#${cmd}"/>
+                </svg>`
+    });
   },
 
   addUiControl: function(cmd, handler) {
