26318b23839ef1c8b79f53a9593cef4f5cdaaae5
Raffi <raffij@gmail.com>
2016-03-17 17:24:16 +0000

    Updating working of multi-editable

--- src/block_mixins/multi-editable.js
+++ src/block_mixins/multi-editable.js
@@ -10,12 +10,21 @@ module.exports = {
     this.editors = {};
   },
 
-  newTextEditor: function(template, content) {
-    // render template outside of dom
-    var wrapper = document.createElement('div');
-    wrapper.innerHTML = template;
+  newTextEditor: function(template_or_node, content) {
+    var editor, isTextTemplate;
 
-    var editor = wrapper.querySelector('.st-block__editor');
+    isTextTemplate = (template_or_node.tagName === undefined);
+
+    if (isTextTemplate) {
+      // render template outside of dom
+      var wrapper = document.createElement('div');
+      wrapper.innerHTML = template_or_node;
+
+      editor = wrapper.querySelector('.st-block__editor');
+    } else {
+      editor = template_or_node;
+    }
+
     var id = _.uniqueId('editor-');
     editor.setAttribute('data-editorId', id);
     editor.addEventListener('keyup', this.getSelectionForFormatter);
@@ -30,7 +39,7 @@ module.exports = {
     scribe.setContent(content);
 
     var editorObject = {
-      node: wrapper.removeChild(wrapper.firstChild),
+      node: isTextTemplate ? wrapper.removeChild(wrapper.firstChild) : editor,
       el: editor,
       scribe: scribe,
       id: id
