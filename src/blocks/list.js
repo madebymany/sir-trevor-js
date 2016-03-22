b747210107537cb8f2595d0596c90f2ba6621799
Raffi <raffij@gmail.com>
2016-03-22 10:20:40 +0000

    Add listType to list blocks

--- src/blocks/list.js
+++ src/blocks/list.js
@@ -2,6 +2,7 @@
 
 var Block = require('../block');
 var stToHTML = require('../to-html');
+var Dom = require('../packages/dom');
 
 var ScribeListBlockPlugin = require('./scribe-plugins/scribe-list-block-plugin');
 
@@ -9,7 +10,22 @@ module.exports = Block.extend({
   type: 'list',
   title: function() { return i18n.t('blocks:list:title'); },
   icon_name: 'list',
+
+  controllable: true,
   multi_editable: true,
+
+  controls: {
+    'unorderedlist': function(e) {
+      e.preventDefault();
+
+      this.updateListType('unordered');
+    },
+    'orderedlist': function(e) {
+      e.preventDefault();
+
+      this.updateListType('ordered');
+    }
+  },
 
   scribeOptions: { 
     allowBlockElements: false,
@@ -22,7 +38,20 @@ module.exports = Block.extend({
     scribe.use(new ScribeListBlockPlugin(this));
   },
 
-  editorHTML: '<ul class="st-list-block__list"></ul>',
+  listTagName: function() {
+    return (this.listType === 'ordered') ? 'ol' : 'ul';
+  },
+
+  editorHTML: function() {
+    return this.createRootNode().outerHTML;
+  },
+
+  createRootNode: function() {
+    return Dom.createElement(this.listTagName(), {
+      'class': 'st-list-block__list'
+    })
+  },
+
   listItemEditorHTML: '<li class="st-list-block__item"><div class="st-list-block__editor st-block__editor"></div></li>',
 
   initialize: function() {
@@ -37,26 +66,39 @@ module.exports = Block.extend({
   },
 
   onBlockRender: function() {
-    if (!this.ul) { this.setupListVariables(); }
+    if (!this.list) { this.setupListVariables(); }
     if (this.editorIds.length < 1) { this.addListItem(); }
   },
 
   setupListVariables: function() {
-    this.ul = this.inner.querySelector('ul');
+    this.list = this.inner.querySelector('ul, ol');
   },
 
   loadData: function(data) {
-    var block = this;
     if (this.options.convertFromMarkdown && data.format !== "html") {
       data = this.parseFromMarkdown(data.text);
     }
 
+    while(Object.keys(this.editors).length) {
+      var item = this.editors[Object.keys(this.editors)[0]];
+
+      this.editorIds.splice(0, 1);
+      this.list.removeChild(item.node);
+      this.removeTextEditor(item.id);
+    }
+
+    this.listType = data.listType || 'unordered';
+
+    Dom.replaceWith(this.list, this.createRootNode());
+
+    this.setupListVariables();
+    
     if (data.listItems.length) {
-      data.listItems.forEach(function(li) {
-        block.addListItem(li.content);
+      data.listItems.forEach((li) => {
+        this.addListItem(li.content);
       });
     } else {
-      block.addListItem();
+      this.addListItem();
     }
   },
 
@@ -74,10 +116,16 @@ module.exports = Block.extend({
   },
 
   _serializeData: function() {
-    var data = {format: 'html', listItems: []};
+    var data = {
+      format: 'html', 
+      listItems: [], 
+      listType: this.listType
+    };
 
     this.editorIds.forEach(function(editorId) {
-      var listItem = {content: this.getTextEditor(editorId).scribe.getContent()};
+      var listItem = {
+        content: this.getTextEditor(editorId).scribe.getContent()
+      };
       data.listItems.push(listItem);
     }.bind(this));
 
@@ -94,15 +142,14 @@ module.exports = Block.extend({
     if (content.trim() === "<br>") { content = ''; }
 
     var editor = this.newTextEditor(this.listItemEditorHTML, content);
-
-    if (after && this.ul.lastchild !== after.node) {
+    if (after && this.list.lastchild !== after.node) {
       var before = after.node.nextSibling;
-      this.ul.insertBefore(editor.node, before);
+      this.list.insertBefore(editor.node, before);
 
       var idx = this.editorIds.indexOf(after.id) + 1;
       this.editorIds.splice(idx, 0, editor.id);
     } else {
-      this.ul.appendChild(editor.node);
+      this.list.appendChild(editor.node);
       this.editorIds.push(editor.id);
     }
 
@@ -147,7 +194,7 @@ module.exports = Block.extend({
 
     this.focusOnNeighbor(item);
     this.editorIds.splice(idx, 1);
-    this.ul.removeChild(item.node);
+    this.list.removeChild(item.node);
     this.removeTextEditor(item.id);
   },
 
@@ -179,6 +226,12 @@ module.exports = Block.extend({
     } else {
       return null;
     }
+  },
+
+  updateListType: function(listType) {
+    this.setAndLoadData({
+      listType: listType
+    });
   }
 
 });
