391e1426fa4e74cadf0ea1960461e3395bc54658
Devindra <arden.ived@gmail.com>
2020-04-28 16:23:53 +0100

    Add 3 levels to the heading block

--- src/blocks/heading.js
+++ src/blocks/heading.js
@@ -8,18 +8,26 @@ var Block = require('../block');
 var stToHTML = require('../to-html');
 
 var ScribeTextBlockPlugin = require('./scribe-plugins/scribe-text-block-plugin');
-var ScribeHeadingPlugin = require('./scribe-plugins/scribe-heading-plugin');
 var ScribeQuotePlugin = require('./scribe-plugins/scribe-quote-plugin');
+var ScribeHeadingLevelPlugin = require('./scribe-plugins/scribe-heading-level-plugin');
+var ScribeHeadingLevel2Plugin = ScribeHeadingLevelPlugin(2);
+var ScribeHeadingLevel3Plugin = ScribeHeadingLevelPlugin(3);
+var ScribeHeadingLevel4Plugin = ScribeHeadingLevelPlugin(4);
 
 module.exports = Block.extend({
 
   type: 'heading',
 
-  editorHTML: '<h2 class="st-required st-text-block st-text-block--heading" contenteditable="true"></h2>',
+  editorHTML: function() {
+    var data = this.getBlockData();
+    return `<h${data.level} class="st-required st-text-block st-text-block--heading" contenteditable="true"></h${data.level}>`;
+  },
 
   configureScribe: function(scribe) {
+    scribe.use(new ScribeHeadingLevel2Plugin(this));
+    scribe.use(new ScribeHeadingLevel3Plugin(this));
+    scribe.use(new ScribeHeadingLevel4Plugin(this));
     scribe.use(new ScribeTextBlockPlugin(this));
-    scribe.use(new ScribeHeadingPlugin(this));
     scribe.use(new ScribeQuotePlugin(this));
 
     scribe.on('content-changed', this.toggleEmptyClass.bind(this));
@@ -38,7 +46,7 @@ module.exports = Block.extend({
 
   icon_name: 'heading',
 
-  loadData: function(data){
+  loadData: function(data) {
     if (this.options.convertFromMarkdown && data.format !== "html") {
       this.setTextBlockHTML(stToHTML(data.text, this.type));
     } else {
@@ -56,6 +64,6 @@ module.exports = Block.extend({
 
   asClipboardHTML: function() {
     var data = this.getBlockData();
-    return `<h2>${data.text}</h2>`;
+    return `<h${data.level}>${data.text}</h${data.level}>`;
   }
 });
