c52185200cecbba88b0b2828b998dfd8df2bad53
Devindra <arden.ived@gmail.com>
2020-05-04 15:24:05 +0100

    Fix drop title fetching

--- src/config.js
+++ src/config.js
@@ -1,12 +1,10 @@
 "use strict";
 
 var drop_options = {
-  title: function() {
-    return _.result(block, "title")
-  },
+  title: null,
   html: ['<div class="st-block__dropzone">',
     '<svg role="img" class="st-icon"><use xlink:href="<%= config.defaults.iconUrl %>#<%= _.result(block, "icon_name") %>"/></svg>',
-    '<p><%= i18n.t("general:drop", { block: "<span>" + _.result(block, "drop_options.title") + "</span>" }) %>',
+    '<p><%= i18n.t("general:drop", { block: "<span>" + _.result(_.result(block, "drop_options"), "title") + "</span>" }) %>',
     '</p></div>'].join('\n'),
   re_render_on_reorder: false
 };
